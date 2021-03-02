c8f6e289727a3967c71060ed91895f881932fa64
BillyCoding <gustavo.rocha.191066@hotmail.com>
2021-03-02 20:07:23 -0300

    Start: Initialize App

---
+++ src/main/java/com/springmvc/curso/springmvccurso/SpringmvcCursoApplication.java
@@ -0,0 +1,13 @@
+package com.springmvc.curso.springmvccurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springmvc/curso/springmvccurso/SpringmvcCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvc.curso.springmvccurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
